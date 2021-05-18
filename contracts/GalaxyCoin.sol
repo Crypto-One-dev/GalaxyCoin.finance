338b734c665e3fe8e0e102887ec4dff31abf5fa6
Nishant <nishantbijani29@gmail.com>
2021-05-18 14:52:40 +0530

    Update transferFrom method

--- contracts/GalaxyCoin.sol
+++ contracts/GalaxyCoin.sol
@@ -1,4 +1,5 @@
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
+
 //SPDX-License-Identifier: MIT
 
 /***
@@ -25,25 +26,19 @@ Taxes :
 import "./Ownable.sol";
 import "./SafeMath.sol";
 import "./IERC20.sol";
-  
 
-contract GALAXYCOIN is IERC20, Ownable{
-    
+
+contract GALAXYCOIN is IERC20, Ownable {
     using SafeMath for uint256;
-    mapping (address => uint256) public _balances;
-    mapping (address => mapping (address => uint256)) private _allowances;
-    
-    
-    uint256 private _totalSupply;
+      
     string  private _name;
     string  private _symbol;
+    
+    uint256 private _totalSupply;
     uint256 private _decimals = 7; // number count after decimal point
-
     uint256 private constant _MAX = ~uint256(0);
     uint256 private _DECIMALFACTOR = 10 ** uint256(_decimals);
-   
     uint256 private _tTotal = 100000 * _DECIMALFACTOR;
-
 
     // Addresses   
     uint256 public adminFeePercent;
@@ -53,7 +48,11 @@ contract GALAXYCOIN is IERC20, Ownable{
     address private _adminTwoTokenHolder; // Second admin token holder address
     address private _liquidityTransfer; // Liquidity transfer address
 
-    constructor (address _admin1TokensHolder, address _admin2TokensHolder, address _liquidityTransfer, uint256 _adminFeePercent, uint256 _liquidityFeePercent)  {
+    mapping (address => uint256) public _balances;
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+
+    constructor (address _admin1TokensHolder, address _admin2TokensHolder, address _liquiditytransfer, uint256 _adminFeePercent, uint256 _liquidityFeePercent)  {
         _name = 'Galaxy Coin';
         _symbol = 'GLXY';
         _totalSupply =  55000000  * _DECIMALFACTOR;
@@ -61,13 +60,15 @@ contract GALAXYCOIN is IERC20, Ownable{
         // Initialize token holder addresses
         _adminOneTokenHolder = _admin1TokensHolder;
         _adminTwoTokenHolder = _admin2TokensHolder;
-        _liquidityTransfer = _liquidityTransfer;
+        _liquidityTransfer = _liquiditytransfer;
         
         // Define admin and liquidity fee in percent
         adminFeePercent = _adminFeePercent; // convert into admin fee percent 
         liquidityFeePercent = _liquidityFeePercent;
+        
         //transfer total supply to owner
         _balances[_msgSender()] = _totalSupply;
+
         emit Transfer(address(this), _msgSender(),  _totalSupply);
     }
 
@@ -83,8 +84,7 @@ contract GALAXYCOIN is IERC20, Ownable{
         return _decimals;
     }
 
-    function totalSupply() public view override returns (uint256) {
-        
+    function totalSupply() public view override returns (uint256) { 
         return _totalSupply;
     }
 
@@ -93,21 +93,19 @@ contract GALAXYCOIN is IERC20, Ownable{
     }
     
     function find1Percent(uint256 value, uint256 percent) public pure returns (uint256)  {
-         uint256 onePercent = value.mul(percent).div(10000);
-         return onePercent;
+        require((value.div(10000)).mul(10000) == value, 'too small');
+        uint256 onePercent = value.mul(percent).div(10000);
+        return onePercent;
     }
-
     
     function transfer(address recipient, uint256 amount) public  override returns (bool) {
         _transfer(msg.sender, recipient, amount);
         return true;
     }
-
    
     function allowance(address owner, address spender) public view virtual override returns (uint256) {
         return _allowances[owner][spender];
     }
-
  
     function approve(address spender, uint256 amount) public  virtual override returns (bool) {
         _approve(msg.sender, spender, amount);
@@ -115,25 +113,20 @@ contract GALAXYCOIN is IERC20, Ownable{
     }
 
     function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
-        require(_allowances[sender][msg.sender] >= amount, "Insufficient allowance");
+        require(_allowances[sender][msg.sender] >= amount, "In Sufficient allowance");
         
-        uint256 tokensToAdmin1 = find1Percent(amount, adminFeePercent);
+        uint256 tokensToAdmin = find1Percent(amount, adminFeePercent);
         uint256 tokensToLiquidity = find1Percent(amount, liquidityFeePercent);
-        uint256 tokensToTransfer = amount.sub(tokensToAdmin1).sub(tokensToAdmin1).sub(tokensToAdmin1).sub(tokensToLiquidity);
-    
+        uint256 tokensToTransfer = amount.sub(tokensToAdmin).sub(tokensToAdmin).sub(tokensToLiquidity);
 
-        _balances[_adminOneTokenHolder] = _balances[_adminOneTokenHolder].add(tokensToAdmin1);
-        _balances[_adminTwoTokenHolder] = _balances[_adminTwoTokenHolder].add(tokensToAdmin1);
-        _balances[_liquidityTransfer] = _balances[_liquidityTransfer].add(tokensToLiquidity);
-             
-        
-
-        emit Transfer(sender, _adminOneTokenHolder, tokensToAdmin1 );
-        emit Transfer(sender, _adminTwoTokenHolder, tokensToAdmin1 );
-        emit Transfer(sender, _liquidityTransfer, tokensToLiquidity );
+        _transfer(sender, _adminOneTokenHolder, tokensToAdmin);
+        _transfer(sender, _adminTwoTokenHolder, tokensToAdmin);
+        _transfer(sender, _liquidityTransfer, tokensToLiquidity);
 
         _transfer(sender, recipient, tokensToTransfer);
-        _approve(sender,msg.sender, _allowances[sender][msg.sender]-=amount);
+        _allowances[sender][msg.sender] = _allowances[sender][msg.sender].sub(amount);
+        _approve(sender,msg.sender, _allowances[sender][msg.sender]);
+        
         return true;
     }
 
@@ -141,10 +134,11 @@ contract GALAXYCOIN is IERC20, Ownable{
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
         require(sender != recipient,"cannot send money to your Self");
-        require(_balances[sender]>=amount,"Insufficient Funds");
+        require(_balances[sender]>=amount,"In Sufficiebt Funds");
         
         uint256 senderBalance = _balances[sender];
         require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+        
         _balances[sender] = senderBalance - amount;
         _balances[recipient] += amount;
         
@@ -156,9 +150,10 @@ contract GALAXYCOIN is IERC20, Ownable{
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
         require(owner != spender,"cannot send allowances to yourself");
-        require(_balances[owner]>=amount,"Insufficient Funds");
+        require(_balances[owner]>=amount,"In Sufficiebt Funds");
     
         _allowances[owner][spender] = amount;
+
         emit Approval(owner, spender, amount);
     }
     
@@ -167,7 +162,6 @@ contract GALAXYCOIN is IERC20, Ownable{
         require(msg.sender == _adminOneTokenHolder, "Only admin is allowed");
         _;
     }
-  
 
     // function to update LiquidityTransfer
     function UpdateLiquidityTransfer(address liquidityTransfer) external onlyAdmin {
@@ -177,6 +171,5 @@ contract GALAXYCOIN is IERC20, Ownable{
     receive()
         payable
         external {
-        }
-        
+    }   
 }
